d156f9f6db242e31640b69a7bf6a8429843baa37
Khanh Nguyen Dinh <khanhnd@ikameglobal.com>
2024-06-12 22:11:22 +0700

    add sample

---
+++ Assets/_Project/Scripts/Advertisement/SampleSceneManager.cs
@@ -0,0 +1,25 @@
+﻿using Pancake.Monetization;
+using Pancake.Scriptable;
+using UnityEngine;
+
+namespace _Project.Scripts.Advertisement
+{
+    public class SampleSceneManager : MonoBehaviour
+    {
+        [SerializeField] private ApplovinInterVariable interVariable;
+        [SerializeField] private ScriptableEventString changeNetworkEvent;
+
+        [ContextMenu("Show Inter")]
+        public void ShowInter()
+        {
+            Debug.Log("Show Interstitial");
+            interVariable.Show();
+        }
+
+        [ContextMenu("Change Admob")]
+        public void ChangeAdmob()
+        {
+            changeNetworkEvent.Raise("admob");
+        }
+    }
+}
